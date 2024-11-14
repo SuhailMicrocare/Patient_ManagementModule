ff33e7ba1aed5a5d49e78a2698583444c86966bb
SuhailMicrocare <msuhail@microcare.io>
2024-11-14 17:03:19 +0530

    Complete Responsive Work

--- javascript/patientdashbord.js
+++ javascript/patientdashbord.js
@@ -1,29 +1,93 @@
 $(document).ready(function () {
-  const $listItems = $(".p-navigation ul li");
+  const $desktopListItems = $(".p-navigation__desktop-list li");
+  const $modalListItems = $(".p-navigation__modal-list li");
   const $divs = $(
     ".personal-infomaindiv, .Insurance-infodiv, .Noninsurance-infodiv, .Dependencies-infodiv, .Attachment-infodiv, .Audittrail-infodiv, .Appoinment-infodiv, .Visits-infodiv, .Questionire-infodiv, .Registration-infodiv, .Locations-infodiv"
   );
+  const $navigationToggle = $(".p-navigation__toggle");
+  const $modal = $(".p-navigation__modal");
+  const $modalClose = $(".p-navigation__modal-close");
+  const $selectedText = $(".p-navigation__selected-text");
 
-  // Initially, apply the 'selected' class to the first list item and show the first div
-  $listItems.first().addClass("selected");
+  // Initially, apply the 'selected' class to the first list items and show the first div
+  $desktopListItems.first().addClass("selected");
+  $modalListItems.first().addClass("selected");
   $divs.first().show();
 
-  // Hide all other divs except .personal-infomaindiv on page load
+  // Hide all other divs except the first one
   $divs.not(":first").hide();
 
-  // Add event listener to each list item
-  $listItems.on("click", function () {
-    // Remove 'selected' class from all items
-    $listItems.removeClass("selected");
+  // Open modal
+  $navigationToggle.on("click", function () {
+    $modal.addClass("show");
+    $("body").addClass("modal-open");
+  });
 
-    // Add 'selected' class to the clicked item
-    $(this).addClass("selected");
+  // Close modal
+  function closeModal() {
+    $modal.removeClass("show");
+    $("body").removeClass("modal-open");
+  }
 
-    // Hide all divs
+  $modalClose.on("click", closeModal);
+
+  // Close modal when clicking outside the content
+  $modal.on("click", function (event) {
+    if ($(event.target).is($modal)) {
+      closeModal();
+    }
+  });
+
+  // Handle desktop list item clicks
+  $desktopListItems.on("click", function () {
+    const $this = $(this);
+    const selectedText = $this.text();
+    const index = $desktopListItems.index(this);
+
+    // Update desktop list
+    $desktopListItems.removeClass("selected");
+    $this.addClass("selected");
+
+    // Update modal list
+    $modalListItems.removeClass("selected");
+    $modalListItems.eq(index).addClass("selected");
+
+    // Update selected text and content
+    updateSelection(selectedText, index);
+  });
+
+  // Handle modal list item clicks
+  $modalListItems.on("click", function () {
+    const $this = $(this);
+    const selectedText = $this.text();
+    const index = $modalListItems.index(this);
+
+    // Update modal list
+    $modalListItems.removeClass("selected");
+    $this.addClass("selected");
+
+    // Update desktop list
+    $desktopListItems.removeClass("selected");
+    $desktopListItems.eq(index).addClass("selected");
+
+    // Update selected text and content
+    updateSelection(selectedText, index);
+
+    // Close modal
+    closeModal();
+  });
+
+  // Helper function to update selection
+  function updateSelection(selectedText, index) {
+    $selectedText.text(selectedText);
     $divs.hide();
+    $divs.eq(index).show();
+  }
 
-    // Show the corresponding div
-    const index = $listItems.index(this); // Get the index of the clicked item
-    $divs.eq(index).show(); // Show the corresponding div
+  // Close modal on escape key
+  $(document).on("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeModal();
+    }
   });
 });
